be6f5693d2b53828e315c65cc6b8b48f4c31f8c5
b17010476 <abdelbens13@outlook.fr>
2018-11-26 17:07:56 +0100

    create getter and setter of width and height

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -1,35 +1,67 @@
 package image;
 
 import javafx.scene.paint.Color;
+import util.Matrices;
 
 public class BruteRasterImage implements Image{
-    Color color;
+    Color[][] pixels;
     int width;
     int height;
 
+
     public BruteRasterImage(Color color, int width, int height) {
-        this.color = color;
         this.width = width;
         this.height = height;
+        pixels = new Color[width][height];
+        for(int x=0;x<width;x++){
+            for(int y=0;y<height;y++){
+                pixels[x][y]=color;
+            }
+        }
     }
     public BruteRasterImage(Color[][]colors){
+        Matrices.requiresNonZeroDimensions(colors);
 
     }
 
     @Override
-    public javafx.scene.paint.Color getPixelColor(int x, int y) {
-        return null;
+    public Color getPixelColor(int x, int y) {
+        return this.pixels[x][y];
     }
 
     @Override
     public int getWidth() {
-        return 0;
+        return this.width;
     }
 
     @Override
     public int getHeight() {
-        return 0;
+        return this.height;
     }
 
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setPixelColor(Color color, int x,int y) {
+        for (x=0;x<getWidth();x++){
+            for(y=0;y<getHeight();y++){
+                pixels[x][y] = color;
+            }
+        }
+    }
+    public void setPixelsColor(Color[][]pixels){
+
+    }
+    public void setPixelsColor(Color color){
+
+    }
+    public void createRepresentation(){
+
+    }
 }
 
